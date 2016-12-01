ce40ce965f1716408c7e9187ed9d1c34a50cc299
Evan Lezar <evanlezar@gmail.com>
2016-12-01 19:22:58 +0100

    Improve oh-my-zsh install

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+OH_MY_ZSH_PATH=$HOME/.oh-my-zsh
+
+if [ ! -d $OH_MY_ZSH_PATH ]
+then
+git clone git://github.com/robbyrussell/oh-my-zsh.git $OH_MY_ZSH_PATH
+fi
+
+echo "› upgrade oh-my-zsh"
+env ZSH=$OH_MY_ZSH_PATH DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh -f $OH_MY_ZSH_PATH/tools/check_for_upgrade.sh
+
+
